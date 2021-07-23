bef754ba73b436c7bc2f6f209ba3c65c9fc94ab4
John Strode <jdstrode@gmail.com>
2021-07-22 19:01:13 -0500

    deleted unneeded files

--- app_files/static/js/app_mapping.js
+++ app_files/static/js/app_mapping.js
@@ -1,10 +1,3 @@
-// let mapdata = d3.select("api/mapdata")
-// console.log(mapdata)
-
-// d3.json("api/mapdata").then(incomingData => {
-//     console.log(incomingData);
-
-// });
 
 // Define streetmap layer
 var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
@@ -44,10 +37,52 @@ L.control.layers(baseMaps, overlayMaps, {
 }).addTo(myMap);
 
 function init() {
+  d3.json("api/mapdata").then((incomingData) => {
+      console.log(incomingData)
+      let yearlyDeforestData = incomingData[0]["2004"];
+      console.log(yearlyDeforestData);
+      let latitudes = incomingData[0].Latitude;
+      console.log(latitudes);
+      let longitudes = incomingData[0].Longitude;
+      console.log(longitudes);
+      let state = incomingData[0].States;
+      console.log(state);
+
+      for (var i = 0; i < longitudes.length-1; i++) {
+
+      let circle = L.circle([latitudes[i],longitudes[i]], {
+        // color: 'red',
+        // fillColor: '#f03',
+        color: circleColor(yearlyDeforestData[i]),
+        fillOpacity: 0.5,
+        radius: yearlyDeforestData[i] * 50
+      // }).addTo(myMap);
+
+  }).bindPopup("<h4> State: " + state[i] + "</h4> <hr> <h5>Deforestation: " + yearlyDeforestData[i] + "</h5>").addTo(deforestation);
+
+  deforestation.addTo(myMap);
+  
+}});
+}
+
+init();
+
+
+
+d3.selectAll("#selDataset").on("change", getData);
+
+function getData() {
+
+  let dropdownMenu = d3.select("#selDataset");
+  var selector = dropdownMenu.property("value");
+
+  console.log(selector)
+
+  var data = [];
+  console.log(data);
+  deforestation.clearLayers();
     d3.json("api/mapdata").then((incomingData) => {
         console.log(incomingData)
-        let yearlyDeforestData = incomingData[0]["2004"];
-        console.log(yearlyDeforestData);
         let latitudes = incomingData[0].Latitude;
         console.log(latitudes);
         let longitudes = incomingData[0].Longitude;
@@ -55,25 +90,71 @@ function init() {
         let state = incomingData[0].States;
         console.log(state);
 
+        if (selector == "2004") {
+          data = incomingData[0]["2004"];
+          console.log(data);
+        }
+        else if (selector == "2005") {
+            data = incomingData[0]["2005"];
+        }
+        else if (selector == "2006") {
+            data =  incomingData[0]["2006"];
+        }
+        else if (selector == "2007") {
+            data =  incomingData[0]["2007"];
+        }
+        else if (selector == "2008") {
+            data =  incomingData[0]["2008"];
+        }
+        else if (selector == "2009") {
+            data =  incomingData[0]["2009"];
+        }
+        else if (selector == "2010") {
+            data =  incomingData[0]["2010"];
+        }
+        else if (selector == "2011") {
+            data =  incomingData[0]["2011"];
+        }
+        else if (selector == "2012") {
+            data =  incomingData[0]["2012"];
+        }
+        else if (selector == "2013") {
+            data =  incomingData[0]["2013"];
+        }
+        else if (selector == "2014") {
+          data =  incomingData[0]["2014"];
+        }
+        else if (selector == "2015") {
+          data =  incomingData[0]["2015"];
+        }
+        else if (selector == "2016") {
+          data =  incomingData[0]["2016"];
+        }
+        else if (selector == "2017") {
+          data =  incomingData[0]["2017"];
+        }
+        else if (selector == "2018") {
+          data =  incomingData[0]["2018"];
+        }
+        else if (selector == "2019") {
+          data =  incomingData[0]["2019"];
+        }
+
         for (var i = 0; i < longitudes.length-1; i++) {
 
         let circle = L.circle([latitudes[i],longitudes[i]], {
           // color: 'red',
           // fillColor: '#f03',
-          color: circleColor(yearlyDeforestData[i]),
+          color: circleColor(data[i]),
           fillOpacity: 0.5,
-          radius: yearlyDeforestData[i] * 50
+          radius: data[i] * 50
         // }).addTo(myMap);
 
-    }).bindPopup("<h4> State: " + state[i] + "</h4> <hr> <h5>Deforestation: " + yearlyDeforestData[i] + "</h5>").addTo(deforestation);
+    }).bindPopup("<h4> State: " + state[i] + "</h4> <hr> <h5>Deforestation: " + data[i] + "</h5>").addTo(deforestation);
 
     deforestation.addTo(myMap);
   }});
 }
-
-init();
-
-
 
   function circleColor(deforest) {
     if (deforest > 2000) {
@@ -87,5 +168,3 @@ init();
     }
     return color; 
 };
-
-
